ef7b75dcb1ea87a2e452b391628576a9239f4a21
Anil Kunwar <anilkunwar318@yahoo.com>
2018-11-22 06:37:33 +0100

    Update ButlerVolmerKinetics.C

--- src/kernel/ButlerVolmerKinetics.C
+++ src/kernel/ButlerVolmerKinetics.C
@@ -21,8 +21,9 @@ validParams<ButlerVolmerKinetics>()
 // the primary variable u is eta 
   params.addRequiredCoupledVar(
       "c_pot", "potential as coupled variable");
+// the secondary variable v2 is also eta
   params.addRequiredCoupledVar(
-          "mu", "chemical potential as coupled variable 2");
+          "c_eta", "coupled eta as coupled variable 2");
   params.addRequiredParam<MaterialPropertyName>(
           "f_name", "Base name of the free energy function F defined in a DerivativeParsedMaterial");
   return params;
@@ -32,12 +33,12 @@ ButlerVolmerKinetics::ButlerVolmerKinetics(const InputParameters & parameters)
 : DerivativeMaterialInterface<Kernel>(parameters),
 	_c_pot_var(coupled("c_pot")),
 	_c_pot(coupledValue("c_pot")),
-  _mu_var(coupled("mu")),
-	_mu(coupledValue("mu")),
+  _c_eta_var(coupled("c_eta")),
+	_c_eta(coupledValue("c_eta")),
   _F(getMaterialProperty<Real>("f_name")),
   //_dFe(getMaterialPropertyDerivative<Real>("f_name", _var.name())),
   _dF(getMaterialPropertyDerivative<Real>("f_name", _var.name())),
-  _dF_mu(getMaterialPropertyDerivative<Real>("f_name", getVar("mu", 0)->name())),
+  _dF_c_eta(getMaterialPropertyDerivative<Real>("f_name", getVar("c_eta", 0)->name())),
 	_dF_c_pot(getMaterialPropertyDerivative<Real>("f_name", getVar("c_pot", 0)->name()))
 {
 }
@@ -59,8 +60,8 @@ ButlerVolmerKinetics::computeQpOffDiagJacobian(unsigned int jvar)
 {
    if (jvar == _c_pot_var)
 	   return _dF_c_pot[_qp]*_phi[_j][_qp]*_test[_i][_qp];
-    else if (jvar == _mu_var)
-      return _dF_mu[_qp]*_phi[_j][_qp]*_test[_i][_qp];
+    else if (jvar == _c_eta_var)
+      return _dF_c_eta[_qp]*_phi[_j][_qp]*_test[_i][_qp];
 	else
      return 0;
 }
