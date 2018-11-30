0f1f5a97a4ee912cb66dbdba94ce63b2b14b4c3c
Anil Kunwar <anilkunwar318@yahoo.com>
2018-11-30 06:22:11 +0100

    Create IonConcentration.C

---
+++ src/auxkernels/IonConcentration.C
@@ -0,0 +1,68 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "IonConcentration.h"
+
+registerMooseObject("NewtApp", IonConcentration);
+
+template <>
+InputParameters
+validParams<IonConcentration>()
+{
+  InputParameters params = validParams<AuxKernel>();
+
+  // Declare the options for a MooseEnum.
+  // These options will be presented to the user in Peacock
+  // and if something other than these options is in the input file
+  // an error will be printed
+  //MooseEnum component("x y z");
+
+  // Use the MooseEnum to add a parameter called "component"
+  //params.addRequiredParam<MooseEnum>("component", component, "The desired component of velocity.");
+
+  // Add a "coupling paramater" to get a variable from the input file.
+  params.addRequiredCoupledVar("chem_pot", "chemical potential as coupled variable.");
+
+  return params;
+}
+
+IonConcentration::IonConcentration(const InputParameters & parameters)
+  : AuxKernel(parameters),
+
+    // This will automatically convert the MooseEnum to an integer
+    //_component(getParam<MooseEnum>("component")),
+   
+
+     // We can couple in a value from one of our kernels with a call to coupledValueAux
+    _chempot(coupledValue("chem_pot")),
+
+    // Get the gradient of the variable
+    _chempot_gradient(coupledGradient("chem_pot")),
+
+    // Set reference to the permeability MaterialProperty.
+    // Only AuxKernels operating on Elemental Auxiliary Variables can do this
+    //_permeability(getMaterialProperty<Real>("permeability")),
+
+    // Set reference to the viscosity MaterialProperty.
+    // Only AuxKernels operating on Elemental Auxiliary Variables can do this
+    //_viscosity(getMaterialProperty<Real>("viscosity"))
+    _ionconc(getMaterialProperty<Real>("ion-concentration"))
+{
+}
+
+Real
+IonConcentration::computeValue()
+{
+  // Access the gradient of the pressure at this quadrature point
+  // Then pull out the "component" of it we are looking for (x, y or z)
+  // Note that getting a particular component of a gradient is done using the
+  // parenthesis operator
+  //return -(_permeability[_qp] / _viscosity[_qp]) * _pressure_gradient[_qp](_component);
+  return _ionconc[_qp] ;
+}
