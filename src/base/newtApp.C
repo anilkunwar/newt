53f150430f41a5e24f8b296a5898078993d90d25
Anil Kunwar <anilkunwar318@yahoo.com>
2023-04-11 20:51:07 +0200

    Update newtApp.C

--- src/base/newtApp.C
+++ src/base/newtApp.C
@@ -4,30 +4,30 @@
 #include "ModulesApp.h"
 #include "MooseSyntax.h"
 
-template <>
 InputParameters
-validParams<newtApp>()
+newtApp::validParams()
 {
-  InputParameters params = validParams<MooseApp>();
+  InputParameters params = MooseApp::validParams();
+
   return params;
 }
 
 newtApp::newtApp(InputParameters parameters) : MooseApp(parameters)
 {
-  Moose::registerObjects(_factory);
-  ModulesApp::registerObjects(_factory);
-  newtApp::registerObjects(_factory);
-
-  Moose::associateSyntax(_syntax, _action_factory);
-  ModulesApp::associateSyntax(_syntax, _action_factory);
-  newtApp::associateSyntax(_syntax, _action_factory);
-
-  Moose::registerExecFlags(_factory);
-  ModulesApp::registerExecFlags(_factory);
-  newtApp::registerExecFlags(_factory);
+  newtApp::registerAll(_factory, _action_factory, _syntax);
 }
 
 newtApp::~newtApp() {}
+
+void
+newtApp::registerAll(Factory & f, ActionFactory & af, Syntax & syntax)
+{
+  ModulesApp::registerAll(f, af, syntax);
+  Registry::registerObjectsTo(f, {"newtApp"});
+  Registry::registerActionsTo(af, {"newtApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
 
 void
 newtApp::registerApps()
@@ -35,59 +35,16 @@ newtApp::registerApps()
   registerApp(newtApp);
 }
 
-void
-newtApp::registerObjects(Factory & factory)
-{
-    Registry::registerObjectsTo(factory, {"newtApp"});
-}
-
-void
-newtApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & action_factory)
-{
-  Registry::registerActionsTo(action_factory, {"newtApp"});
-
-  /* Uncomment Syntax parameter and register your new production objects here! */
-}
-
-void
-newtApp::registerObjectDepends(Factory & /*factory*/)
-{
-}
-
-void
-newtApp::associateSyntaxDepends(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
-{
-}
-
-void
-newtApp::registerExecFlags(Factory & /*factory*/)
-{
-  /* Uncomment Factory parameter and register your new execution flags here! */
-}
-
 /***************************************************************************************************
  *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
  **************************************************************************************************/
 extern "C" void
+newtApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  newtApp::registerAll(f, af, s);
+}
+extern "C" void
 newtApp__registerApps()
 {
   newtApp::registerApps();
-}
-
-extern "C" void
-newtApp__registerObjects(Factory & factory)
-{
-  newtApp::registerObjects(factory);
-}
-
-extern "C" void
-newtApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory)
-{
-  newtApp::associateSyntax(syntax, action_factory);
-}
-
-extern "C" void
-newtApp__registerExecFlags(Factory & factory)
-{
-  newtApp::registerExecFlags(factory);
 }
